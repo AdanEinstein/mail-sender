cefba2079a243fe198a4d21c8b723d0bc63bdbc5
AdanEinstein <pr219543@prodesan.com.br>
2023-06-28 15:41:18 -0300

    Confiurations

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='send-mail',
+    name='mail-sender-1.0',
     version='1.0',
     author='AdanEinstein',
     author_email='adaneinstein@gmail.com',
