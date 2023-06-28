6c3633dbff9fb6e8575b726908291d2eb6a51cb7
AdanEinstein <pr219543@prodesan.com.br>
2023-06-28 14:30:53 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='send-mail',
+    version='1.0',
+    author='Adan Einstein',
+    author_email='adaneinstein@gmail.com',
+    packages=find_packages()
+)
+
